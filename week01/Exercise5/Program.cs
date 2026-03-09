097d8c8c0b3f4fb0eec99122857d4339b12214ad
ohotuowo-morgan <125ohotumorgan@gmail.com>
2026-03-09 23:51:01 +0100

    Week 1 complete C# practice

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // static void Mainn(string PromptUserName(); int PromptUserNumber(); int SquareFavNumber();)
+        // {
+        //     DisplayWelcome();
+
+        //     Console.WriteLine($"{userName} the square of your number {newNum} is {sqrNum}");
+        // }
+
+        DisplayWelcome();
+
+        string name = PromptUserName();
+
+        int favNum = PromptUserNumber();
+
+        int sqr = SquareFavNumber(favNum);
+
+        DisplayResult(name, favNum, sqr);
+
+    }
+
+    static void DisplayResult(string name, int favNum, int sqr)
+    {
+        Console.WriteLine($"{name}, the square of your Favourite number {favNum} is {sqr}");
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string favNum = Console.ReadLine();
+        int newNum = int.Parse(favNum);
+        return newNum;
+    }
+
+    static int SquareFavNumber(int newNum)
+    {
+        int sqrNum = newNum * newNum;
+        return sqrNum;
     }
 }
