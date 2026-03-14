927ae66b98bb19d7ce031a0b870d0cd543be3da2
ohotuowo-morgan <125ohotumorgan@gmail.com>
2026-03-14 01:36:17 +0100

    week2 abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2002;
+        job1._endYear = 2024;
+        
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2028;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Adrián San Miguel";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        
+        // Console.WriteLine(resume1._jobs[0]._jobTitle);
+
+        resume1.DisplayResumeInfo();
     }
 }
