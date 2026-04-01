86640c0ace3901f4d263df78d089745a58551596
ohotuowo-morgan <125ohotumorgan@gmail.com>
2026-04-01 12:52:49 +0100

    Week 5 homework project

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathAssignment = new MathAssignment("Morgan", "Algebra", "7.2", "21-30");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Emma", "Literature", "Irony of Laughter");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
