46265d5d888154fea0555212f02470a5a137d338
ohotuowo-morgan <125ohotumorgan@gmail.com>
2026-04-16 19:10:47 +0100

    week7 exercise tracking

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+         Running runningActivity = new Running("03 Nov 2022", 30, 3.0);
+        Cycling cyclingActivity = new Cycling("04 Nov 2022", 45, 15.0);
+        Swimming swimmingActivity = new Swimming("05 Nov 2022", 20, 40);
+
+        List<Activity> activities = new List<Activity>();
+        activities.Add(runningActivity);
+        activities.Add(cyclingActivity);
+        activities.Add(swimmingActivity);
+
+        Console.WriteLine("Exercise Tracking Summary:\n");
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
